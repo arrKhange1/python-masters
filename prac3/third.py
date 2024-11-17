cf82eaba4c886cf82bfc06287ed602c088280f38
arrKhange1 <therock65frostik@gmail.com>
2024-11-17 22:09:47 +0500

    feat: add 3rd layer

--- prac3/third.py
+++ prac3/third.py
@@ -10,31 +10,59 @@ class ServoDrive:
         return f'ServoDrive(model={self.model!r}, power={self.power!r})'
     
 
-class SynchronousServoDrive(ServoDrive):
-    def __init__(self, model, power, synchronization_time):
+class SpinnableServoDrive(ServoDrive):
+    def __init__(self, model, power, max_speed, direction):
         super().__init__(model, power)
+        self.max_speed = max_speed
+        self.direction = direction
+
+    def __str__(self):
+        return f'SpinnableServoDrive(model={self.model}, power={self.power}, max_speed={self.max_speed}, direction={self.direction})'
+
+    def __repr__(self):
+        return f'SpinnableServoDrive(model={self.model!r}, power={self.power!r}, max_speed={self.max_speed!r}, direction={self.direction!r})'
+
+class SynchronousServoDrive(SpinnableServoDrive):
+    def __init__(self, model, power, max_speed, direction, synchronization_time):
+        super().__init__(model, power, max_speed, direction)
         self.synchronization_time = synchronization_time
 
     def __str__(self):
-        return f'SynchronousServoDrive(model={self.model}, power={self.power}, synchronization_time={self.synchronization_time})'
+        return f'SynchronousServoDrive(model={self.model},\
+            power={self.power},\
+            synchronization_time={self.synchronization_time}),\
+            max_speed={self.max_speed},\
+            direction={self.direction}'
 
     def __repr__(self):
-        return f'SynchronousServoDrive(model={self.model!r}, power={self.power!r}, synchronization_time={self.synchronization_time!r})'
+        return f'SynchronousServoDrive(model={self.model!r},\
+            power={self.power!r},\
+            synchronization_time={self.synchronization_time!r}),\
+            max_speed={self.max_speed!r},\
+            direction={self.direction!r}'
     
-class AsynchronousServoDrive(ServoDrive):
-    def __init__(self, model, power, async_jobs):
-        super().__init__(model, power)
+class AsynchronousServoDrive(SpinnableServoDrive):
+    def __init__(self, model, power, max_speed, direction, async_jobs):
+        super().__init__(model, power, max_speed, direction)
         self.async_jobs = async_jobs
 
     def __str__(self):
-        return f'AsynchronousServoDrive(model={self.model}, power={self.power}, async_jobs={self.async_jobs})'
+        return f'SynchronousServoDrive(model={self.model},\
+            power={self.power},\
+            async_jobs={self.async_jobs}),\
+            max_speed={self.max_speed},\
+            direction={self.direction}'
 
     def __repr__(self):
-        return f'SynchronousServoDrive(model={self.model!r}, power={self.power!r}, async_jobs={self.async_jobs!r})'
+        return f'SynchronousServoDrive(model={self.model!r},\
+            power={self.power!r},\
+            async_jobs={self.async_jobs!r}),\
+            max_speed={self.max_speed!r},\
+            direction={self.direction!r}'
 
 
 servo = ServoDrive('Servo 1000', 1000)
-syncServo = SynchronousServoDrive('Sync Servo 1000', 1000, 10)
-asyncServo = AsynchronousServoDrive('Aync Servo 1000', 1000, 150)
+syncServo = SynchronousServoDrive('Sync Servo 1000', 1000, 1500, 1, 10)
+asyncServo = AsynchronousServoDrive('Async Servo 1000', 1000, 1500, -1, 15)
 
 print(asyncServo)
