7f9a055b16706f5389beac3f8886f921dc48b429
arrKhange1 <therock65frostik@gmail.com>
2024-11-17 21:59:14 +0500

    feat: add 2 levels of hierarchy

--- prac3/third.py
+++ prac3/third.py
@@ -10,3 +10,31 @@ class ServoDrive:
         return f'ServoDrive(model={self.model!r}, power={self.power!r})'
     
 
+class SynchronousServoDrive(ServoDrive):
+    def __init__(self, model, power, synchronization_time):
+        super().__init__(model, power)
+        self.synchronization_time = synchronization_time
+
+    def __str__(self):
+        return f'SynchronousServoDrive(model={self.model}, power={self.power}, synchronization_time={self.synchronization_time})'
+
+    def __repr__(self):
+        return f'SynchronousServoDrive(model={self.model!r}, power={self.power!r}, synchronization_time={self.synchronization_time!r})'
+    
+class AsynchronousServoDrive(ServoDrive):
+    def __init__(self, model, power, async_jobs):
+        super().__init__(model, power)
+        self.async_jobs = async_jobs
+
+    def __str__(self):
+        return f'AsynchronousServoDrive(model={self.model}, power={self.power}, async_jobs={self.async_jobs})'
+
+    def __repr__(self):
+        return f'SynchronousServoDrive(model={self.model!r}, power={self.power!r}, async_jobs={self.async_jobs!r})'
+
+
+servo = ServoDrive('Servo 1000', 1000)
+syncServo = SynchronousServoDrive('Sync Servo 1000', 1000, 10)
+asyncServo = AsynchronousServoDrive('Aync Servo 1000', 1000, 150)
+
+print(asyncServo)
