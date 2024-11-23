4e741d037e5a4371433fffc10ace6ca15005ad5b
arrKhange1 <therock65frostik@gmail.com>
2024-11-23 22:01:22 +0500

    refact: add id logic, change add logic

--- lab2/first.py
+++ lab2/first.py
@@ -3,13 +3,11 @@ from typing import List, Dict, Any
 
 class TaskManager:
     tasks: List[Dict[str, any]] = [
-        { 'id': 1, 'is_done': 1, 'description': 'Finished task', 'category': 'pstu'},
-        { 'id': 2, 'is_done': 0, 'description': 'Not finished task', 'category': 'work'},
-        { 'id': 3, 'is_done': 0, 'description': 'Yet another not finished task', 'category': 'work'},
     ]
 
     def add(self, task):
-        self.tasks.append(task)
+        new_task_id = self.tasks[-1]['id']+1 if len(self.tasks) else 1
+        self.tasks.append({**task, 'is_done': 0, 'id': new_task_id})
     
     def remove(self, id):
         self.tasks = [task for task in self.tasks if task.id != id]
@@ -25,23 +23,24 @@ class TaskManager:
             print(f'{i+1}. [{'x' if task['is_done'] else ''}] {task['description']} #{task['category']}')
 
 class TaskTracker:
-
     taskManager = TaskManager()
 
     def start(self):
         while(True):
             self.taskManager.print_tasks()
-            print('\nOptions:\n1. Add task\n2. Export as JSON\n3. Toggle task status\n')
-            match int(input('Enter desired option: \n')): 
-                case 1:
+            print('\nOptions:\n1. Add task\n2. Export as JSON\n3. Toggle task status\n4. Quit')
+            match input('Enter desired option: \n'): 
+                case '1':
                     self.taskManager.add({
-                            'is_done': 0,
                             'description': input('Enter task description: '), 
                             'category': input('Enter task category: ')})
-                case 2:
+                case '2':
                     self.taskManager.export_as_json()
-                case 3:
+                case '3':
                     self.taskManager.toggle_task_status(int(input('Enter task ID to toggle its status: ')))
+                case '4':
+                    print('Task Tracker finished work')
+                    return
 
 taskTracker = TaskTracker()
 taskTracker.start()
